89ea6e45477123c2f3063f4a220ad5669a649331
Ayman H <aymen9255@gmail.com>
2024-08-02 23:26:27 +0300

    Editing some stuff in settings form

--- frmSettings.Designer.cs
+++ frmSettings.Designer.cs
@@ -28,14 +28,43 @@
         /// </summary>
         private void InitializeComponent()
         {
+            MaterialSkin.MaterialListBoxItem materialListBoxItem1 = new MaterialSkin.MaterialListBoxItem();
+            MaterialSkin.MaterialListBoxItem materialListBoxItem2 = new MaterialSkin.MaterialListBoxItem();
             System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSettings));
+            this.colorDialog1 = new System.Windows.Forms.ColorDialog();
+            this.lbTest = new MaterialSkin.Controls.MaterialListBox();
             this.SuspendLayout();
+            // 
+            // lbTest
+            // 
+            this.lbTest.BackColor = System.Drawing.Color.Gray;
+            this.lbTest.BorderColor = System.Drawing.Color.LightGray;
+            this.lbTest.Depth = 0;
+            this.lbTest.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lbTest.Font = new System.Drawing.Font("Cascadia Mono", 18F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            materialListBoxItem1.SecondaryText = "";
+            materialListBoxItem1.Tag = null;
+            materialListBoxItem1.Text = "First";
+            materialListBoxItem2.SecondaryText = "";
+            materialListBoxItem2.Tag = null;
+            materialListBoxItem2.Text = "Colors";
+            this.lbTest.Items.Add(materialListBoxItem1);
+            this.lbTest.Items.Add(materialListBoxItem2);
+            this.lbTest.Location = new System.Drawing.Point(0, 0);
+            this.lbTest.MouseState = MaterialSkin.MouseState.HOVER;
+            this.lbTest.Name = "lbTest";
+            this.lbTest.SelectedIndex = -1;
+            this.lbTest.SelectedItem = null;
+            this.lbTest.Size = new System.Drawing.Size(280, 551);
+            this.lbTest.TabIndex = 2;
+            this.lbTest.SelectedIndexChanged += new MaterialSkin.Controls.MaterialListBox.SelectedIndexChangedEventHandler(this.lbTest_SelectedIndexChanged);
             // 
             // frmSettings
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(474, 620);
+            this.ClientSize = new System.Drawing.Size(653, 551);
+            this.Controls.Add(this.lbTest);
             this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
             this.Name = "frmSettings";
             this.Text = "Settings";
@@ -44,5 +73,7 @@
         }
 
         #endregion
+        private System.Windows.Forms.ColorDialog colorDialog1;
+        private MaterialSkin.Controls.MaterialListBox lbTest;
     }
 }
--- frmSettings.cs
+++ frmSettings.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using MaterialSkin;
 
 namespace TypeTest
 {
@@ -15,6 +16,11 @@ namespace TypeTest
         public frmSettings()
         {
             InitializeComponent();
+        }
+
+
+        private void lbTest_SelectedIndexChanged(object sender, MaterialListBoxItem selectedItem)
+        {
         }
     }
 }
