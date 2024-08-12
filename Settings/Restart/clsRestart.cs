2ab08b825098a5f671c55876f5eea07e08d97f24
Ayman H <aymen9255@gmail.com>
2024-08-12 12:09:50 +0300

    Initial Restart

---
+++ Settings/Restart/clsRestart.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TypeTest.Settings.Restart
+{
+    public class clsRestart
+    {
+        MainForm form;
+
+        
+        public clsRestart(MainForm Form)
+        {
+            form = Form;
+
+            RestartAll();
+        }
+
+        public void RestartAll()
+        {
+            
+        }
+    }
+}
