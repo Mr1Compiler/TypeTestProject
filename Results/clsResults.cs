7c31df610753f83d7e55f76cdf15915b8d5282ba
Ayman H <aymen9255@gmail.com>
2024-08-12 20:11:30 +0300

    Initializing Results form and class

---
+++ Results/clsResults.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TypeTest.Colors;
+using TypeTest.Keyboard;
+using TypeTest.Paragraphs;
+using TypeTest.Settings;
+using TypeTest.Settings.Restart;
+using TypeTest.Settings.Timer;
+
+namespace TypeTest.End_Screen
+{
+    public class clsResults
+    {
+
+    }
+}
